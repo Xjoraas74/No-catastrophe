50d3e4a5066840a675f50bd29f842cc4092a17c5
Xjoraas74 <xjoraas@gmail.com>
2020-06-11 19:17:43 +0700

    Hopefully added .gitignore. Added player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 movement;
+
+    public Rigidbody2D rb;
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
