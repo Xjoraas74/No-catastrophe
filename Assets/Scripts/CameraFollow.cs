7346cca88d22dbacaa8245fa9b46fee8671eeb62
Xjoraas74 <xjoraas@gmail.com>
2020-06-13 15:32:29 +0700

    Make camera follow Player, import cinemachine
    Do we need cinemachine?..

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    public Camera cam;
+
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.position = transform.position + offset;
+    }
+}
