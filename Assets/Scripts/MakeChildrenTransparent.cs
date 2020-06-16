81278f79de935d351b695ba26db6c366f7488581
Xjoraas74 <xjoraas@gmail.com>
2020-06-16 17:10:05 +0700

    Make objects transparent on enter a place
    Step on pizza in the sample scene to see how it works

---
+++ Assets/Scripts/MakeChildrenTransparent.cs
@@ -0,0 +1,78 @@
+﻿// https://www.youtube.com/watch?v=nNjNWDZSkAI
+
+using UnityEngine;
+
+public class MakeChildrenTransparent : MonoBehaviour
+{
+    public GameObject toFade;// = null;
+    GameObject[] listToFade;
+
+    void Start()
+    {
+        listToFade = new GameObject[toFade.transform.childCount];
+        Component[] transforms = GetComponentsInChildren<Transform>(toFade);
+        for (int i = 0; i < listToFade.Length; i++)
+        {
+            listToFade[i] = transforms[i].gameObject;
+        }
+    }
+
+    private bool IsCharacter(Collider2D collider)
+    {
+        // Implement you logic here if it is your player that is the collider
+        return true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (IsCharacter(collider))
+        {
+            foreach (GameObject i in listToFade)
+            {
+                SetMaterialTransparent(i);
+                iTween.FadeTo(toFade, 0, 1);
+            }
+        }
+    }
+
+    private void SetMaterialTransparent(GameObject toChange)
+    {
+        foreach (Material m in toChange.GetComponent<Renderer>().materials)
+        {
+            m.SetFloat("_Mode", 2);
+            m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            m.SetInt("_ZWrite", 0);
+            m.DisableKeyword("_ALPHATEST_ON");
+            m.EnableKeyword("_ALPHABLEND_ON");
+            m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            m.renderQueue = 3000;
+        }
+    }
+
+    private void SetMaterialOpaque(GameObject toChange)
+    {
+        foreach (Material m in toChange.GetComponent<Renderer>().materials)
+        {
+            m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            m.SetInt("_ZWrite", 1);
+            m.DisableKeyword("_ALPHATEST_ON");
+            m.DisableKeyword("_ALPHABLEND_ON");
+            m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            m.renderQueue = -1;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (IsCharacter(collider))
+        {
+            foreach (GameObject i in listToFade)
+            {
+                iTween.FadeTo(toFade, 1, 1);
+                Invoke("SetMaterialOpaque", 1.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MakeTransparent.cs
@@ -0,0 +1,62 @@
+﻿// https://www.youtube.com/watch?v=nNjNWDZSkAI
+
+using UnityEngine;
+
+public class MakeTransparent : MonoBehaviour
+{
+    public GameObject ToFade = null;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (IsCharacter(collider))
+        {
+            SetMaterialTransparent();
+            iTween.FadeTo(ToFade, 0, 1);
+        }
+    }
+
+    private bool IsCharacter(Collider2D collider)
+    {
+        // Implement you logic here if it is your player that is the collider
+        return true;
+    }
+
+    private void SetMaterialTransparent()
+    {
+        foreach (Material m in ToFade.GetComponent<Renderer>().materials)
+        {
+            m.SetFloat("_Mode", 2);
+            m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            m.SetInt("_ZWrite", 0);
+            m.DisableKeyword("_ALPHATEST_ON");
+            m.EnableKeyword("_ALPHABLEND_ON");
+            m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            m.renderQueue = 3000;
+        }
+    }
+
+    private void SetMaterialOpaque()
+    {
+        foreach (Material m in ToFade.GetComponent<Renderer>().materials)
+        {
+            m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            m.SetInt("_ZWrite", 1);
+            m.DisableKeyword("_ALPHATEST_ON");
+            m.DisableKeyword("_ALPHABLEND_ON");
+            m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            m.renderQueue = -1;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (IsCharacter(collider))
+        {
+            // Set material to opaque
+            iTween.FadeTo(ToFade, 1, 1);
+            Invoke("SetMaterialOpaque", 1.0f);
+        }
+    }
+}
