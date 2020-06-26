b14845ed9f704e826a749aff3ced523365295311
Xjoraas74 <xjoraas@gmail.com>
2020-06-26 17:54:21 +0700

    Add basic dialog to resedential floor

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    public float typeSpeed = .02f;
+    public GameObject continueButton;
+
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textDisplay.text = "";
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char symbol in sentences[index].ToCharArray())
+        {
+            textDisplay.text += symbol;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+
+    public void TypeNewSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
