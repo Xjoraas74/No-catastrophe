f74884cd262b3ccc12b789dfc908f488a7153721
Xjoraas74 <xjoraas@gmail.com>
2020-07-02 12:19:32 +0700

    Create countdown timer for the game

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = float.Parse(text.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        text.text = time.ToString("0");
+
+        if (time < 0)
+        {
+            Application.Quit();
+        }
+    }
+}
