6a033e86da4ef4d5394bf613fa0357fbc685da94
Xjoraas74 <xjoraas@gmail.com>
2020-07-07 20:25:17 +0700

    Add UI for changing scenes
    And add UI canvas to the science floor

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    //public static bool gameIsPaused;
+    public bool activatable = true;
+
+    void Update()
+    {
+        //if (activatable)
+        //{
+        //    Invoke("Activate", .25f);
+        //}
+        Time.timeScale = 0f;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("running");
+            Time.timeScale = 1f;
+            gameObject.SetActive(false);
+        }
+        //Debug.Log(gameObject.activeSelf);
+        //Debug.Log(activatable);
+
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    gameIsPaused = !gameIsPaused;
+        //    PauseGame();
+        //}
+    }
+
+    //void PauseGame()
+    //{
+    //    if (gameIsPaused)
+    //    {
+    //        gameObject.SetActive(true);
+    //        Time.timeScale = 0f;
+    //    }
+    //    else
+    //    {
+    //        gameObject.SetActive(false);
+    //        Time.timeScale = 1;
+    //    }
+    //}
+
+    public void LoadResidentialFloor()
+    {
+        SceneManager.LoadScene("Residential floor");
+    }
+
+    public void LoadScienceFloor()
+    {
+        SceneManager.LoadScene("Science floor");
+    }
+
+    public void LoadTechnicalFloor()
+    {
+        SceneManager.LoadScene("Technical floor");
+    }
+
+    //void Activate()
+    //{
+    //    Time.timeScale = 0f;
+    //    activatable = false;
+    //}
+}
---
+++ Assets/Scripts/ChangeSceneTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+    public GameObject changeScene;
+
+    void OnTriggerEnter2D()
+    {
+        changeScene.SetActive(true);
+        //Debug.Log("KAPETS");
+    }
+
+    void OnTriggerExit2d()
+    {
+        changeScene.GetComponent<ChangeScene>().activatable = true;
+    }
+}
