a9748257908ce675f526fc5b7c7d6eff6b61f0f9
tanya_kleynos <tanyakleynos@gmail.com>
2020-07-09 22:52:03 +0700

    Player Animation (with and without mask)

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        animator.SetBool("mask", false);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("moveUp", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool("moveUp", false);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("moveUp", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool("moveUp", true);
+        }
+    }
+}
