198e5fea5d75ce6ca166d50a71f6567520405a38
Ari1i1 <afanask1nanyc@gmail.com>
2020-07-10 23:09:18 +0700

    just to share

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class Click : MonoBehaviour
+public class Click : MonoBehaviour, IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click is");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
